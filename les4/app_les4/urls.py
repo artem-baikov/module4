7caec83317068a8d29fb5021e37b3e0a279d60a1
tommotello <artyom.baykov.l@gmail.com>
2023-07-14 03:11:15 +0500

    +templates +static

--- les4/app_les4/urls.py
+++ les4/app_les4/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers, register, profile, login, advertisement, advertisement_post
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='main-page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
+    path('login/', login, name='login'),
+    path('advertisement/', advertisement, name='advertisement'),
+    path('advertisement-post/', advertisement_post, name='advertisement-post'),
+
 ]
--- les4/app_les4/views.py
+++ les4/app_les4/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(requests):
-    return HttpResponse('Success')
+def index(request):
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def advertisement(request):
+    return render(request, 'advertisement.html')
+
+def advertisement_post(request):
+    return render(request, 'advertisement-post.html')
